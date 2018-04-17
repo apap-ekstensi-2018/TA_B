f22988a7a6a137314f0697243a5a4c17baf4a431
Poppy Rosaline <poppy.rosaline@ui.ac.id>
2018-04-17 21:09:58 +0700

    Tugasv1

---
+++ src/main/java/com/example/ProjectApapApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApapApplication.class, args);
+	}
+}
