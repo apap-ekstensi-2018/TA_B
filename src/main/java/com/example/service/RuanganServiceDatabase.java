233ab6281fe91b6418bd501d116c399aab6b02e0
shafamp <shafamaharani1995@gmail.com>
2018-05-01 14:36:02 +0700

    added fitur ruangan

--- src/main/java/com/example/service/RuanganServiceDatabase.java
+++ src/main/java/com/example/service/RuanganServiceDatabase.java
@@ -2,8 +2,10 @@ package com.example.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.dao.RuanganMapper;
 import com.example.model.RuanganModel;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,18 +13,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class RuanganServiceDatabase implements RuanganService{
+	
+	@Autowired
+    private RuanganMapper ruanganMapper;
 
 	@Override
-	public RuanganModel selectRuangan(int id_ruangan) {
-		// TODO Auto-generated method stub
-		return null;
+	public RuanganModel selectRuangan(int id_ruang) {
+			return ruanganMapper.selectRuangan(id_ruang);
 	}
 
 	@Override
 	public List<RuanganModel> selectAllRuangans() {
-		// TODO Auto-generated method stub
-		return null;
+        return ruanganMapper.selectAllRuangans ();
 	}
+	
 
 	@Override
 	public void addRuangan(RuanganModel ruanganModel) {
@@ -37,7 +41,7 @@ public class RuanganServiceDatabase implements RuanganService{
 	}
 
 	@Override
-	public void deleteRuangan(int id_ruangan) {
+	public void deleteRuangan(int id_ruang) {
 		// TODO Auto-generated method stub
 		
 	}
