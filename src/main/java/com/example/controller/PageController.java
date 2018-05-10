6117acf2dd5e0db241eed85d025cbf3ecafb85ee
selina.octavia <selinaoctavia2@gmail.com>
2018-05-10 18:33:45 +0700

    fixed login trouble

--- src/main/java/com/example/controller/PageController.java
+++ src/main/java/com/example/controller/PageController.java
@@ -1,13 +1,20 @@
 package com.example.controller;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 	
 	@RequestMapping("/")
-	public String index () {
+	public String index (/*Model model*/) {
+		//model.addAttribute("role", getRole());
 		return "index";
 	}
 	
@@ -15,4 +22,19 @@ public class PageController {
 	public String login () {
 		return "login";
 	}
+	
+	public String getRole(){
+        String role = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            Collection<? extends GrantedAuthority> grantedAuthorities = ((UserDetails)principal).getAuthorities();
+            for (GrantedAuthority a : grantedAuthorities) {
+                role = a.getAuthority();	
+            }
+        } else {
+            role = principal.toString();
+        }
+
+        return role;
+    }
 }
